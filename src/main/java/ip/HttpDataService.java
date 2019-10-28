758db3641fbffa20ab40135543092f0e7b12a783
Nicolas <nicolas.gamel12450@gmail.com>
2019-10-28 17:29:41 +0100

    suite tp

--- src/main/java/ip/HttpDataService.java
+++ src/main/java/ip/HttpDataService.java
@@ -5,18 +5,13 @@ import java.io.IOException;
 import org.apache.http.client.fluent.Request;
 
 public class HttpDataService {
-	
 	public static String url = "http://jsonip.com";
-	
+
 	public String getJsonIp() {
 		try {
-			
 			return Request.Get(url).execute().returnContent().toString();
-		
 		} catch (IOException e) {
-			
 			throw new RuntimeException(e);
-		
 		}
 	}
 }
--- src/main/java/ip/MyIpService.java
+++ src/main/java/ip/MyIpService.java
@@ -5,23 +5,17 @@ import java.util.Map;
 import com.google.gson.Gson;
 
 public class MyIpService {
-	
 	private HttpDataService httpDataService;
-	
+
 	public MyIpService(HttpDataService httpDataService) {
-		
 		this.httpDataService = httpDataService;
-		
-	}
-	
-	public String getMyIp() {
-		
-		Gson gson = new Gson();
-		String jsonIp = httpDataService.getJsonIp();
-		Map<String, String> map = gson.<Map<String, String>>fromJson(jsonIp, Map.class);
-		
-		return map.get("ip").split(",")[0];
-	
 	}
 
+	public String getMyIp() {
+		Gson gson = new Gson();
+		String jsonIp = httpDataService.getJsonIp();
+		System.out.println(jsonIp);
+		Map<String, String> map = gson.<Map<String, String>>fromJson(jsonIp, Map.class);
+		return map.get("ip").split(",")[0];
+	}
 }
